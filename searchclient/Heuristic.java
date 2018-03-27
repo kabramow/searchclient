7b62ae2317a0f8657ae1b86989854fbbd45c785f
kabramow <kabramow@indiana.edu>
2018-03-27 14:04:36 +0200

    started point distance construction - still need to validate if (0,0) is a wall or implemented BFS

--- searchclient/Heuristic.java
+++ searchclient/Heuristic.java
@@ -10,11 +10,31 @@ import searchclient.NotImplementedException;
 public abstract class Heuristic implements Comparator<Node> {
 	//character is a goal represent by an lowercase letter, and the value is is a list of locations of that goal
 	HashMap<Character, ArrayList<Point>> goalLocations;
-	public Heuristic(Node initialState, ArrayList<ArrayList<Character>> goals) {
+	//a grid where the values for each part of the grid are also a grid and that grid maps the distance from the point
+	// referenced by the higher grid
+	/*        0 1 2
+			0 X + +
+			1 + + +
+			2 + + +
+
+			In the X grid the following grid is stored
+
+			0 1 2
+			1 2 3
+			2 3 4
+	 */
+	ArrayList<ArrayList<ArrayList<ArrayList<Integer>>>> pointDistances;
+	public Heuristic(Node initialState, ArrayList<ArrayList<Character>> goals, ArrayList<ArrayList<Boolean>> walls) {
 		// Here's a chance to pre-process the static parts of the level.
 
 		//make hashmap of goals for easy look up of nearest goal
 		goalLocations = new HashMap<>();
+
+        pointDistances = new ArrayList<>();
+
+        //a filler object if a point is a wall in the point distances array object
+        ArrayList<ArrayList<Integer>> wallFiller = new ArrayList<>();
+
 		//loop through the goals nested array to find goal locations
 		for(int row = 0; row < goals.size(); row++){
 			for(int col = 0; col < goals.get(row).size(); col++){
@@ -36,10 +56,45 @@ public abstract class Heuristic implements Comparator<Node> {
 					//and add updated locations to hashmap
 					goalLocations.put(currentChar, currentLocationsOfGoal);
 				}
+                //determine distance from one point to another for every point in the array
+                ArrayList<ArrayList<ArrayList<Integer>>> currentRow = new ArrayList<>();
+                pointDistances.add(currentRow);
+                //if current value is a wall
+                if(walls.get(row).get(col)){
+                    currentRow.add(wallFiller);
+                }
+                //if it is not a wall then do BFS to find distance from current point to
+                //all other non-wall points
+                else{
+                    //represents the grid that is the distance between current point and other points
+                    ArrayList<ArrayList<Integer>> subGrid = new ArrayList<>();
+                    //if point is not 0,0 we want to back trace already performed calculations
+                    if(row != 0 && col != 0){
+                        for(int i = 0; i <= row; i++){
+                            ArrayList<Integer> subRow = new ArrayList<>();
+                            subGrid.add(subRow);
+                            for(int j = 0; j < col; j++){
+                                subRow.add(distanceBetweenTwoPoints(i, j, row, col));
+                                //ADD these points to a visited tracker for BFS?
+                            }
+                        }
+                    }
+                    //fill in the rest of the grid with BFS
+                    for(int i = row; i < goals.size(); i++){
+                        ArrayList<Integer> subRow = new ArrayList<>();
+                        subGrid.add(subRow);
+                        for(int j = col; j < goals.get(row).size(); j++){
+                            subRow.add(distanceBetweenTwoPoints(i, j, row, col));
+                        }
+                    }
+                    /*for(int row = 0; row < goals.size(); row++){
+			for(int col = 0; col < goals.get(row).size(); col++){*/
+
+                }
 			}
 		}
 		System.err.println(goalLocations.toString());
-		//TODO determine distance from one point to another for every point in the array
+
 	}
 
 	//Upper A is the box
@@ -88,6 +143,10 @@ public abstract class Heuristic implements Comparator<Node> {
 		return (x1 - x2) + (y2 - y1);
 	}
 
+	public int distanceBetweenTwoPoints(int x1, int y1, int x2, int y2){
+	    return pointDistances.get(x1).get(y1).get(x2).get(y2);
+    }
+
 	public String debugMapperToString(Object[] mapper){
 		return "For char " + mapper[0] + " in (" + mapper[1] + ", " + mapper[2]
 				+ ") the closest goal is (" + mapper[3] + ", " + mapper[4] + ") with a distance of " + mapper[5];
@@ -101,8 +160,8 @@ public abstract class Heuristic implements Comparator<Node> {
 	}
 
 	public static class AStar extends Heuristic {
-		public AStar(Node initialState, ArrayList<ArrayList<Character>> goals) {
-			super(initialState, goals);
+		public AStar(Node initialState, ArrayList<ArrayList<Character>> goals, ArrayList<ArrayList<Boolean>> walls) {
+			super(initialState, goals, walls);
 		}
 
 		@Override
@@ -119,8 +178,8 @@ public abstract class Heuristic implements Comparator<Node> {
 	public static class WeightedAStar extends Heuristic {
 		private int W;
 
-		public WeightedAStar(Node initialState, ArrayList<ArrayList<Character>> goals,  int W) {
-			super(initialState, goals);
+		public WeightedAStar(Node initialState, ArrayList<ArrayList<Character>> goals, ArrayList<ArrayList<Boolean>> walls, int W) {
+			super(initialState, goals, walls);
 			this.W = W;
 		}
 
@@ -136,8 +195,8 @@ public abstract class Heuristic implements Comparator<Node> {
 	}
 
 	public static class Greedy extends Heuristic {
-		public Greedy(Node initialState, ArrayList<ArrayList<Character>> goals) {
-			super(initialState, goals);
+		public Greedy(Node initialState, ArrayList<ArrayList<Character>> goals, ArrayList<ArrayList<Boolean>> walls) {
+			super(initialState, goals, walls);
 		}
 
 		@Override
